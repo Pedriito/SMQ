2901a580e57b2a1ff5f0cff36cc41856928bb6bf
ROMER Pierre-Elie <pierre.romer@adn.fr>
2023-02-03 09:39:28 +0100

    Creation Workflow

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import api, { route } from "@forge/api";
 import ForgeUI, { render, AdminPage, Fragment, Text, Button } from "@forge/ui";
-let Onglet = [];
-let Page = {
-  "issuetype": 0 ,
 
-}
+// Variables golables
+
+let issuetypeName;
+let issueTypeSchemeId;
+let issueTypeId;
+let workflowId;
+let workflowName;
 
 
 
@@ -15,125 +18,56 @@ const App = () => {
             <Button text="Click To Install NC Project" onClick={async () => 
               {
               // creation de l'issue de l'issue type 
-              await issuetype("Issue Type NC");
-              // creation de l'issue type scheme 
-              await createissuetypescheme("Kanban Issue Type Scheme NC");
-              // ajout de l'issue type dans le issue type scheme 
-              // await addIssuetypeToscheme(issueTypeSchemeId);
-              // // 
-              // await screentab("Identification et Enregistrement");
-              // await screentab("Cause identifiées");
-              // await screentab("Risques Analysés");
-              // await screentab("Traitement");
-              // await screentab("Validation");
-              // // await getscreens();
-              // await getalltabs();
-
-              // await createfield("Cause(s) identifiée(s)","com.atlassian.jira.plugin.system.customfieldtypes:textarea");
-              // await createfield("Pôle de competence","com.atlassian.jira.plugin.system.customfieldtypes:select");
-              // await createfield("Responsable Analyse","com.atlassian.jira.plugin.system.customfieldtypes:userpicker");
-              // await createfield("Risque","com.atlassian.jira.plugin.system.customfieldtypes:textarea");
-              // await createfield("Besoin CAPA","com.atlassian.jira.plugin.system.customfieldtypes:select");
-              // await createfield("Action (s) réalisée(s) hors CAPA / Justification","com.atlassian.jira.plugin.system.customfieldtypes:textarea");
-              // await createfield("Responsable validation chef de pôle","com.atlassian.jira.plugin.system.customfieldtypes:userpicker");
-              // await createfield("Responsable validation QA","com.atlassian.jira.plugin.system.customfieldtypes:userpicker");
-              // await createfield("ADN_Categorie_NC","com.atlassian.jira.plugin.system.customfieldtypes:select");
-
-              // await getfields();
-              // // // // Identification et Enregistrement
-              // await addFieldToScreenTab(1,Onglet[0],"Résumé");
-              // await addFieldToScreenTab(1,Onglet[0],"Description");
-              // await addFieldToScreenTab(1,Onglet[0],"Responsable");
-              // await addFieldToScreenTab(1,Onglet[0],"ADN_Categorie_NC");
-              // // // Cause identifiées
-              // await addFieldToScreenTab(1,10031,"Cause(s) identifiée(s)");
-              // await addFieldToScreenTab(1,10031,"Pôle de competence");
-              // await addFieldToScreenTab(1,10031,"Responsable Analyse");
-                
-              // // // Risques Analysés
-              // await addFieldToScreenTab(1,Onglet[2],"Risque");
-              // await addFieldToScreenTab(1,Onglet[2], "Besoin CAPA");
-                
-              // // // Traitement
-              // await addFieldToScreenTab(1,Onglet[3],"Action (s) réalisée(s) hors CAPA / Justification");
-              await addFieldToScreenTab(1,Onglet[3], "Pièce jointe");
-              await addFieldToScreenTab(1,Onglet[3], "Tickets liés");
-
-              // // // Validation
-              // await addFieldToScreenTab(1,Onglet[3]),"Responsable validation chef de pôle";
-              // await addFieldToScreenTab(1,Onglet[3],"Responsable validation QA");
-              
+              await createIssueType("Issue Type NC");
+              // creation de l'issue type scheme
+              await createIssueTypeScheme("Kanban Issue type Scheme NC");
+              // créaction du workflow
+              await createWorkflow("Workflow NC");
+              // créaction du workflow scheme
+              await createWorkflowScheme("Workflow Scheme NC");
             }
               }/>
         </Fragment>
     );
 };
-async function addfields(){
 
-}
-async function getfields(){
-  const response = await api.asUser().requestJira(route`/rest/api/2/field`, {
-    headers: {
-      "Accept": "application/json"
-    }
+ // ISSUE TYPE FUNCTIONS 
+
+async function createIssueType(name) {
+  var body = JSON.stringify({
+      name,
+      description: "From FORGE",
+      type: "standard"
   });
-  
-  // ////console.log(`Response: ${response.status} ${response.statusText}`);
-  // //console.log(await response.json());
-    const array_json = await response.json();
-   // Ajouter chaque elements de array_json dans un tableau json
-    const json = [];
-    for (const result of array_json) {
-      json.push(result.id);
-      switch (result.name){
-        case "Cause(s) identifiée(s)" : await addFieldToScreenTab(1,Onglet[1],result.id);
-        case "Pôle de competence": await addFieldToScreenTab(1,Onglet[1],result.id);
-        case "Responsable Analyse": await addFieldToScreenTab(1,Onglet[1],result.id);
-        case "Risque": await addFieldToScreenTab(1,Onglet[2],result.id);
-        case "Besoin CAPA": await addFieldToScreenTab(1,Onglet[2], result.id);
-        case "Action (s) réalisée(s) hors CAPA / Justification":await addFieldToScreenTab(1,Onglet[3],result.id);
-        case "Responsable validation chef de pôle": await addFieldToScreenTab(1,Onglet[3],result.id);
-        case "Responsable validation QA": await addFieldToScreenTab(1,Onglet[3],result.id);
-        case "ADN_Categorie_NC": await addFieldToScreenTab(1,Onglet[0],result.id);
-      }
-    }
-    console.log(`Response: ${response.status} ${response.statusText}`);
-    console.log(response.json);
-    
-}
 
-
-async function issuetype(name) {
-    var bodyData = `{
-        "name": "${name}",
-        "description": "From FORGE ",
-        "type": "standard"
-      }`; 
-      
-      const response = await api.asUser().requestJira(route`/rest/api/3/issuetype`, {
-        method: "POST",
-        headers: {
+  const response = await api.asUser().requestJira(route`/rest/api/3/issuetype`, {
+      method: "POST",
+      headers: {
           "Accept": "application/json",
           "Content-Type": "application/json"
-        },
-        body: bodyData
-      });
-    console.log("Issue Type")
-    console.log(`Response: ${response.status} ${response.statusText}`);
-    console.log(await response.json());  
-    console.log("/////////////////////////");
+      },
+      body
+  });
 
-  }
-  async function createissuetypescheme(name) {
-    var bodyData = `{
-      "defaultIssueTypeId": "10000",
-      "issueTypeIds": [
-      "10000"
-    ],
-    "name": "${name}",
-    "description": "From Forge."
-}`;
+  const json = await response.json();
+  issueTypeId = json.id;
+  issuetypeName = json.name;
+  
+  console.log("issue type created = ", issueTypeId)
 
+
+  console.log(`Response: ${response.status} ${response.statusText}`);
+  console.log("Issue Type created successfully");
+}
+  async function createIssueTypeScheme(name) {
+
+    var bodyData = {
+      defaultIssueTypeId: issueTypeId,
+      issueTypeIds: [issueTypeId],
+      name: name,
+      description: "From Forge.",
+    };
+    
     const response = await api
       .asUser()
       .requestJira(route`/rest/api/3/issuetypescheme`, {
@@ -142,113 +76,88 @@ async function issuetype(name) {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body: bodyData,
+        body: JSON.stringify(bodyData),
       });
-    console.log("Issue Type Scheme");
-    console.log(`Response: ${response.status} ${response.statusText}`);
-    Page.issueTypeSchemeId.push(await response.json());
-    console.log(Page);
-    console.log("/////////////////////////");
     
+    
+      console.log(`Response: ${response.status} ${response.statusText}`);
+    console.log("Issue Type Scheme created succefully");
+    
+    await response.json().then((result) => {
+      issueTypeSchemeId = result;
+    });
+
   }
-  async function addIssuetypeToscheme(issueTypeSchemeId){
-    var bodyData = `{
-      "issueTypeIds": [
-        "10000"
-      ]
-    }`;
+  // WORKFLOW FUNCTIONS 
+  async function createWorkflow(name){
     
-    const response = await api.asUser().requestJira(route`/rest/api/3/issuetypescheme/${issueTypeSchemeId}/issuetype`, {
-      method: 'PUT',
+    var bodyData = JSON.stringify(
+      {
+      "name": name,
+      "description": "Workflow NON CONFORMITE",
+      "statuses": [
+        {
+          "id": "1",
+          "properties": {
+            "jira.issue.editable": "false"
+          }
+        }
+      ],
+      "transitions": [
+        {
+          "name": "Created",
+          "from": [],
+          "to": "1",
+          "type": "initial"
+        }
+      ]
+    }
+    );
+    
+    const response = await api.asUser().requestJira(route`/rest/api/3/workflow`, {
+      method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: bodyData
     });
+    const json = await response.json();
+    workflowName = json.name;
+    workflowId = json.entityId;
+
     
     console.log(`Response: ${response.status} ${response.statusText}`);
-    console.log(await response.json());
+    console.log("Workflow created succefully")
   }
-
-  async function screentab (name){
-    var bodyData = `{
-      "name": "${name}"
-    }`;
     
-    const response = await api.asUser().requestJira(route`/rest/api/3/screens/1/tabs`, {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: bodyData
-    });
-    
-    //console.log(`Response: ${response.status} ${response.statusText}`);
-    //console.log(await response.json());  
 
-  }
-  
-  async function addFieldToScreenTab(screenId, tabId, fieldId) {
-  
-    var bodyData = `{
-      "fieldId": "${fieldId}"
-    }`;
-  
-    const response = await api.asUser().requestJira(route`/rest/api/3/screens/${screenId}/tabs/${tabId}/fields`, {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: bodyData
-    });
-  
-    console.log(`Response: ${response.status} ${response.statusText}`);
-    console.log(await response.json());
-  }
-  
-  async function getalltabs (){
-    const response = await api.asUser().requestJira(route`/rest/api/3/screens/1/tabs`, {
-      headers: {
-        "Accept": "application/json"
+  async function createWorkflowScheme(name){
+    
+    var bodyData = JSON.stringify(
+      {
+      defaultWorkflow: workflowName,
+      name: name ,
+      description: "The description of the NC workflow scheme ",
+      issueTypeMappings: {
+        [issueTypeId] : workflowName,
       }
-    });
-    
-    //console.log(`Response: ${response.status} ${response.statusText}`);
-    //console.log(await response.json());
-    const onglet_id = await response.json();
-    for (const onglet of onglet_id){
-      Onglet.push(onglet.id);
     }
-    console.log(Onglet);
-  }
-
-  async function createfield(name, type, options){
-  
-      var bodyData = `{
-        "name": "${name}",
-        "description": "${type}",
-        "type": "${type}"
-      },`;
-    
- 
- 
-    const response = await api.asUser().requestJira(route`/rest/api/2/field`, {
-      method: "POST",
+    );
+    const response = await api.asUser().requestJira(route`/rest/api/3/workflowscheme`, {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: bodyData,
+      body: bodyData
     });
- 
+
     
-    }
-
-  
-
+    console.log(`Response: ${response.status} ${response.statusText}`);
+    console.log("Workflow Scheme created succefully")
+  }
+ 
 export const run = render(
     <AdminPage>
         <App />
